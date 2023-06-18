383154797ea7ab9ed03529302b2fd5fc9e72dc59
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-06-18 14:15:34 +0530

    initiate the repo

---
+++ app/javascript/controllers/counter_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="counter"
+export default class extends Controller {
+  static targets = ["output"];
+
+  connect() {
+    this.count = 0;
+  }
+
+  increment() {
+    this.count += 1;
+    // console.log(this.count);
+    this.outputTarget.textContent = `You have clicked ${this.count} times`;
+  }
+}
